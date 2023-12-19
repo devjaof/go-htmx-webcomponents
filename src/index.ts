4b20b3c3abd6825d8dc545870e01549deebc98f0
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2023-12-19 16:19:34 -0300

    event listener nos control botoes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 export class ControlButtons extends HTMLElement {
   connectedCallback() {
-    this.querySelector("button#decrease")?.addEventListener("click", () => {
-      console.log('decrease');
+    const decrease = this.querySelector("#decrease") as HTMLButtonElement;
+    const increase = this.querySelector("#increase") as HTMLButtonElement;
+
+    if (!decrease || !increase) {
+      return;
+    }
+
+    decrease.addEventListener("click",function() {
+      if (this.innerText !== "Parar") {
+        this.innerText = "Parar";
+      } else {
+        this.innerText = "Lazer";
+      }
+    })
+
+    increase.addEventListener("click",function() {
+      if (this.innerText !== "Parar") {
+        this.innerText = "Parar";
+      } else {
+        this.innerText = "Produzir";
+      }
     })
   }
 }

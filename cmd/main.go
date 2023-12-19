8f907e0a8353c5c5ba8bfbe8e480bf3187c7aa00
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2023-12-19 12:33:16 -0300

    batata

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "log"
+import "io"
+import "html/template"
+import "github.com/labstack/echo/v4"
+import "github.com/labstack/echo/v4/middleware"
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	templates, err := template.ParseGlob("templates/*.html");
+	if err != nil {
+		log.Fatalf("Failed: %s", err)
+	}
+
+	e := echo.New();
+
+	e.Renderer = &TemplateRenderer{
+		templates: templates,
+	}
+
+	e.Use(middleware.Logger())
+
+	e.Static("/dist", "./dist");
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index.html", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
